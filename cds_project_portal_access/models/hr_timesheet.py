8ee032a33046b4812ad46299ea7bd722fe71ee3b
Moaz Elbahr <moazelbahr@gmail.com>
2026-05-20 11:28:37 +0300

    [FIX] Apply 'Only See Own Timesheets' On All Portal Users Not Only That Has The Group

--- cds_project_portal_access/models/hr_timesheet.py
+++ cds_project_portal_access/models/hr_timesheet.py
@@ -7,7 +7,7 @@ class AccountAnalyticLine(models.Model):
 
     def _timesheet_get_portal_domain(self):
         domain = super()._timesheet_get_portal_domain()
-        if self.env.user.has_group('cds_project_portal_access.group_cds_portal_trainees'):
+        if not self.env.user.has_group('hr_timesheet.group_hr_timesheet_user'):
             if employee := self.env.user.employee_id:
                 domain = Domain(domain) & Domain('employee_id', '=', employee.id)
             else:
